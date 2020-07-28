32a4296f78c5e01030139058077a7e110d95f04c
Dmitry Orlov <me@mosquito.su>
2020-07-29 01:48:46 +0300

    fixes

--- cmagic/__init__.py
+++ cmagic/__init__.py
@@ -3,4 +3,4 @@ from pathlib import Path
 from .version import __author__, __version__        # NOQA
 from ._magic import Magic                           # NOQA
 
-MAGIC_DB = Path(__file__).dirname / "magic.mgc"
+MAGIC_DB = str((Path(__file__).parent / "magic.mgc").resolve())
--- tests/test_magic.py
+++ tests/test_magic.py
@@ -1,3 +1,7 @@
+import os
+
+import pytest
+
 from cmagic import Magic, MAGIC_DB
 
 
@@ -8,5 +12,17 @@ def test_magic():
     assert m.check(MAGIC_DB)
 
     m.load(MAGIC_DB)
+
+    assert "ASCII text" in m.guess_file("/etc/hosts")
+
+
+@pytest.mark.skipif(not os.getenv("MAGIC"), reason="No any env db path")
+def test_magic_env():
+    m = Magic()
+
+    assert m
+    assert m.check()
+
+    m.load()
 
     assert "ASCII text" in m.guess_file("/etc/hosts")
